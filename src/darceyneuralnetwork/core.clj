6f2b35420b31fb2954fb6ac2cf8db0ea20c28a38
Jason Bell <jasebell@gmail.com>
2016-10-18 20:53:40 +0100

    Added classifier options.

--- src/darceyneuralnetwork/core.clj
+++ src/darceyneuralnetwork/core.clj
@@ -7,12 +7,15 @@
 (defn load-instance-data [file-url]
   (wio/load-instances :arff file-url))
 
-(defn train-neural-net [training-data-filename class-index]
+(defn train-neural-net [training-data-filename class-index opts]
   (let [instances (load-instance-data training-data-filename)
-        neuralnet (classifier/make-classifier :neural-network :multilayer-perceptron)]
+        neuralnet (classifier/make-classifier :neural-network :multilayer-perceptron opts)]
     (data/dataset-set-class instances class-index)
     (classifier/classifier-train neuralnet instances)))
 
 (defn build-classifier [training-data-filename output-filename]
-  (let [nnet (train-neural-net training-data-filename)]
+  (let [opts (classifier/make-classifier-options :neural-network :multilayer-perceptron
+                                                 {:learning-rate 0.4
+                                                  :epochs 10000})
+        nnet (train-neural-net training-data-filename 3 opts)]
     (utils/serialize-to-file nnet output-filename)))
